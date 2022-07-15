828c771b2fd2a071484083131131eb283f4510a6
Denise <denisemarielf@gmail.com>
2022-07-14 23:50:48 -0300

    Add ship factory function

---
+++ src/ship-factory.js
@@ -0,0 +1,31 @@
+        //{name: length: 5, x: 0, y: 0, alignment: vertical}
+
+const Ship = (shipLength, shipX, shipY, shipAlignment) => {
+
+  const name = "s"+shipLength;
+  const length = shipLength;
+  const x = shipX;
+  const y = shipY;
+  const alignment = shipAlignment;
+
+  let positions = []
+
+  for (let i = 0; i < shipLength; i++){
+    positions.push(false)
+  }
+  
+  const hit = (position) => {
+    positions[position] = true;
+  }
+
+  const isSunk = () => {
+    const status = positions.every(position => {
+      return position === true}
+      )
+    return status;
+  }
+
+  return {hit, isSunk, positions, name, length, x, y, alignment}
+};
+
+module.exports = Ship;
